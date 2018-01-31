6c078971e49b5a3b8c819d017ed5f99c95c08a0e
tarcisiocorte <tarcisiocorte@gmail.com>
2018-01-31 15:38:03 -0200

    added new migration

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'tccp_financas',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
