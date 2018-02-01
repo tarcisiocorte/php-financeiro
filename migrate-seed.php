3df0e97e84f53980e0c473e89785050a416fbef1
tarcisiocorte <tarcisiocorte@gmail.com>
2018-02-01 09:59:14 -0200

    PHP Faker to increment 10 records

---
+++ migrate-seed.php
@@ -0,0 +1,12 @@
+<?php
+
+exec(__DIR__ . '/vendor/bin/phinx rollback');
+exec(__DIR__ . '/vendor/bin/phinx migrate');
+exec(__DIR__ . '/vendor/bin/phinx seed:run');
+
+
+/************* HOW TO GENERATE FAKE DATA ************* 
+  Use:
+  php migrate-seed.php 
+ 
+*****************************************************/
