951443ba2ff0d8da014cee2d29c35fed7b86d241
tarcisiocorte <tarcisiocorte@gmail.com>
2018-02-22 18:16:25 -0300

    register service

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,8 +1,14 @@
 <?php
+declare(strict_types=1);
+
 namespace TCCP\Plugins;
 
+
 use Aura\Router\RouterContainer;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
 use TCCP\ServiceContainerInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class RoutePlugin implements PluginInterface
 {
@@ -16,10 +22,26 @@ class RoutePlugin implements PluginInterface
         $matcher = $routerContainer->getMatcher();
         /* Tem a funão de gerar links com base nas rotas registradas*/
         $generator = $routerContainer->getGenerator();
+        $request = $this->getRequest();
 
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match($request);
+        });
+
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
 ?>
