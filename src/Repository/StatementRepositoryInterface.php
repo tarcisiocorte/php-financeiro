029d17edcf8fbff9847864ac3f0574b3b36f98b3
tarcisiocorte <tarcisiocorte@gmail.com>
2018-03-02 07:47:35 -0300

    Iniciando consulta de extrato

---
+++ src/Repository/StatementRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace TCCP\Repository;
+
+interface StatementRepositoryInterface
+{
+    public function all(string $dateStart, string $dateEnd, int $userId): array;
+}
